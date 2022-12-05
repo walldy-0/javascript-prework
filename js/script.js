6c7c4458688b1fbc190e52da7c6ce9bccb655546
Waldemar Szymon <walldy@tlen.pl>
2022-12-05 15:32:36 +0100

    Change script.js and index.html

--- js/script.js
+++ js/script.js
@@ -44,10 +44,20 @@ function displayResult(computerMove, playerMove) {
     }
 }
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('Wylosowana liczba to: ' + randomNumber);
-let compMove = GetMoveName(randomNumber);
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Gracz wpisał: ' + playerInput);
+function playGame(playerInput) {
+    clearMessages();
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    console.log('Wylosowana liczba to: ' + randomNumber);
+    console.log('Gracz wybrał: ' + playerInput);
+    displayResult(GetMoveName(randomNumber), GetMoveName(playerInput));
+}
 
-displayResult(compMove, GetMoveName(playerInput));
+document.getElementById('play-rock').addEventListener('click', function() {
+    playGame(1);
+});
+document.getElementById('play-paper').addEventListener('click', function() {
+    playGame(2);
+});
+  document.getElementById('play-scissors').addEventListener('click', function() {
+    playGame(3);
+});
